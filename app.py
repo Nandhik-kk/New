233c2f7335a75b38c274afc957f8de657e2a8f26
Nandhik-kk <jedaiyo23@gmail.com>
2025-04-26 09:41:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,14 @@ st.header("One", divider=True)
 st.header("Two", divider=True)
 st.header("Three", divider=True)
 st.header("Four", divider=True)
+import streamlit as st
+
+st.write("This is some text.")
+
+st.slider("This is a slider", 0, 100, (25, 75))
+
+st.divider()  # 👈 Draws a horizontal rule
+
+st.write("This text is between the horizontal rules.")
+
+st.divider()  # 👈 Another horizontal rule
