7424b11480453de85aba836449f4f1d5c14355f6
Nandhik-kk <jedaiyo23@gmail.com>
2025-04-26 09:48:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,16 @@ st.latex(r'''
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
