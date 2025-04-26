f2d5159ded532a63e46b74163a0185cc8e2fbfff
Nandhik-kk <jedaiyo23@gmail.com>
2025-04-26 09:39:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ import streamlit as st
 
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
