5cdd55a34fcbbea23d019e6d55f569f463775bfc
Nandhik-kk <jedaiyo23@gmail.com>
2025-04-26 10:06:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -100,15 +100,6 @@ with col2:
         disabled=st.session_state.disabled,
         horizontal=st.session_state.horizontal,
     )
-import streamlit as st
-img_file_buffer = st.camera_input("Take a picture")
-
-if img_file_buffer is not None:
-    # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
-    # Check the type of bytes_data:
-    # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
